e3241227e5aaf6ef1e5689c0b82388c0723ff810
Janhavi Palmate <palmatejanhavi@gmail.com>
2026-08-14 14:35:58 +0530

    janhavi

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Programming\Flutter\ai_nexus"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\Programming\Flutter\ai_nexus\macos\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "FLUTTER_ROOT=E:\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\Degree Major project\global-ai-opportunity-tracker-app"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=E:\Degree Major project\global-ai-opportunity-tracker-app\macos\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
